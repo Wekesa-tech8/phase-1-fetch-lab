0861a0a808a734d9f9033fdc47112d3d7741ab46
David Wekesa <wekesadavid300@gmail.com>
2024-07-14 01:22:24 +0300

    Fetch

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  fetch("https://anapioficeandfire.com/api/books")
+    .then((resp) => resp.json()) 
+    .then((json) => renderBooks(json)) 
+    .catch((error) => console.error("Error fetching data:", error)); 
 }
 
 function renderBooks(books) {
-  const main = document.querySelector('main');
-  books.forEach(book => {
-    const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
-    main.appendChild(h2);
+  const booksContainer = document.getElementById('books');
+
+  books.forEach((book) => {
+    const bookItem = document.createElement('li');
+    bookItem.textContent = book.name; 
+    booksContainer.appendChild(bookItem);
   });
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
-});
+document.addEventListener('DOMContentLoaded', fetchBooks);
